d73c7647fee4874369cee811a9c9f14cadf57313
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-09-18 12:29:19 +0530

    added favicon

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./mainHeader.css"
+import "./mainHeader.css";
 function MainHeader() {
   return (
     <>
-      This is the Main Header
       <div className="custom-header">
+        
         <Link to="/">Home</Link>
         <Link to="/about">About Us</Link>
         <Link to="/contact">Contact Us</Link>
