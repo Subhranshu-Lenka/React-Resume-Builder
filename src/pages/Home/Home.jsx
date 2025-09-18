e3740f6581c3c5b5e61695d26e890e02385b8291
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-09-18 21:39:09 +0530

    onto the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h2 className="text-5xl">Build your ATS-beating resume in minutes!</h2>
       <p>Welcome to the Home Page!</p>
     </div>
   );
