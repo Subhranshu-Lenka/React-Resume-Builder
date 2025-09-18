6e079077829969860267e5e516ecac3b04d8d394
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-09-18 23:39:20 +0530

    hero section in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import "./home.css";
 
 function Home() {
   return (
     <div>
-      <h2 className="text-5xl">Build your ATS-beating resume in minutes!</h2>
-      <p>Welcome to the Home Page!</p>
+      {/* <p>Welcome to the Home Page!</p> */}
+      <section className="hero">
+        <h2
+          className="text-5xl text-left"
+        >
+          Build your&nbsp;
+          <span
+            className="italic font-bold">
+            ATS-beating
+          </span> <br />
+          resume in minutes!
+          <br />
+          <p
+            className="mt-5 italic text-lg font-medium">
+            Craft a perfect resume that gets noticed by the recruiters.
+          </p>
+        </h2>
+        <div className="hero-img"></div>
+      </section>
     </div>
   );
 }
