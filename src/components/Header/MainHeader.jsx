09356cede872b396ffb9d5599686b1073f6b29b1
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-09-21 18:42:34 +0530

    refactor: update MainHeader layout and styles for improved navigation

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./mainHeader.css";
 function MainHeader() {
   return (
     <>
-      <div className="custom-header">
+      <div className=" hidden custom-header">
         <NavLink
           to="/"
           className={({ isActive }) => (isActive ? "nav-active" : "")}
@@ -19,8 +18,35 @@ function MainHeader() {
           About Us
         </NavLink>
         <Link to="/contact">Contact Us</Link>
-        <Link to="/contribute">Contribute</Link>
+        <Link to="/contribute">Get Started</Link>
       </div>
+
+      <header className="bg-[rgb(255,255,255,0.1)] 
+      backdrop-blur-[10px] border-b-[1px] border-b-[white]
+      fixed w-full top-0 left-0 z-100
+
+      custom-main-header
+      ">
+        <div className="max-w-[1200px] mx-auto py-[20px] custom-container">
+
+          <nav className="flex justify-between items-center custom-navbar">
+
+            <NavLink to="/"
+              className="text-2xl font-bold">
+              ResumeForge {/* Logo | Home Btn*/}
+            </NavLink>
+
+            <ul className="flex place-content-around gap-12 custom-nav-links">
+              <li> <NavLink to="/template">Templates</NavLink> </li>
+              <li> <NavLink to="/features">Features</NavLink> </li>
+              <li> <NavLink to="/pricing">Pricing</NavLink> </li>
+              <li> <NavLink to="/contact">Contact</NavLink> </li>
+            </ul>
+
+            <NavLink to="/">Get Started</NavLink>
+          </nav>
+        </div>
+      </header>
     </>
   );
 }
