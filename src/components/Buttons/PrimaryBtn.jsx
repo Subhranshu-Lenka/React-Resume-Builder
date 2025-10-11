c996deaff55128f3908a8d966ba4cc59318d244c
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-10-11 12:59:51 +0530

    adding responsive styling

--- src/components/Buttons/PrimaryBtn.jsx
+++ src/components/Buttons/PrimaryBtn.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function PrimaryBtn({ btnLabel = "Label", btnGradient = "", navigationPath }) {
+function PrimaryBtn({ btnLabel = "Label", btnGradient = "", btnStyle = "",navigationPath }) {
   const navigate = useNavigate();
 
   return (
     <button
       onClick={() => navigate(navigationPath)}
-      className={`primary text-lg font-bold px-7 py-[14px] border-none rounded-3xl bg-linear-to-r ${
-        btnGradient ? btnGradient : "from-cyan-700 to-cyan-400"
-      } `}
+      className={`primary text-lg font-bold px-7 py-[14px] border-none rounded-3xl bg-linear-to-r 
+        ${btnGradient ? btnGradient : "from-cyan-700 to-cyan-400"} 
+        ${btnStyle}
+        `}
     >
       {`${btnLabel}`}
     </button>
--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,8 +1,14 @@
 import { Link, NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+
 import "./mainHeader.css";
 import PrimaryBtn from "../Buttons/PrimaryBtn";
 
 function MainHeader({ onFeatureClick }) {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header
@@ -15,12 +21,22 @@ function MainHeader({ onFeatureClick }) {
           className="max-w-[1200px] mx-auto py-[20px]
         custom-container"
         >
-          <nav className="flex justify-between items-center custom-navbar">
+          <nav className="flex justify-between items-center px-4 custom-navbar">
             <NavLink to="/" className="text-2xl font-bold">
               ResumeForge {/* Logo | Home Btn*/}
             </NavLink>
 
-            <ul className="flex place-content-around gap-12 custom-nav-links">
+            <ul
+              className={`
+                 ${
+                   isMenuOpen
+                     ? "flex flex-col gap-8 absolute top-[10vh] right-0 w-fit bg-black shadow-md py-4 px-[16vw]"
+                     : "hidden"
+                 }
+                  md:flex md:flex-row md:items-center md:gap-12 md:static md:bg-transparent md:shadow-none
+                  custom-nav-links
+                `}
+            >
               <li>
                 {" "}
                 <NavLink to="/template">Templates</NavLink>{" "}
@@ -44,8 +60,22 @@ function MainHeader({ onFeatureClick }) {
             <PrimaryBtn
               btnLabel="Get Started"
               btnGradient="from-orange-400 to-amber-700"
+              btnStyle="hidden md:inline-block"
               navigationPath="/login"
             />
+
+            {/* Menu Icon Handler */}
+            <button
+              onClick={() => setMenuOpen(!isMenuOpen)}
+              className="md:hidden"
+            >
+              {isMenuOpen ? (
+                <IoMdClose size={24} />
+              ) : (
+                <GiHamburgerMenu size={24} />
+              )}
+            </button>
+            {/* <GiHamburgerMenu className="h-6 w-auto md:hidden" /> */}
           </nav>
         </div>
       </header>
