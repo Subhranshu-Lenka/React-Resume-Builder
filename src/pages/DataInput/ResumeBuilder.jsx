41763e355322f4308177fa9cb404567f55810386
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-12-17 12:58:04 +0530

    finally workable code

--- src/pages/DataInput/ResumeBuilder.jsx
+++ src/pages/DataInput/ResumeBuilder.jsx
@@ -9,6 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 function ResumeBuilder() {
   const [showPreview, setShowPreview] = useState(false);
+  const [isPreviewActive, setPreviewActive] = useState(false);
 
   const methods = useForm({
     resolver: zodResolver(resumeSchema),
@@ -34,6 +35,8 @@ function ResumeBuilder() {
       ],
     },
   });
+
+  /*TRY 1*/
   // return (
   //   <FormProvider {...methods}>
   //     <div className="grid grid-cols-2 gap-6 p-4 h-screen ">
@@ -47,56 +50,96 @@ function ResumeBuilder() {
   //   </FormProvider>
   // );
 
+  /*TRY 2*/
+  // return (
+  //   <FormProvider {...methods}>
+  //     {/* Large screens: show both form and preview side by side */}
+  //     <div className="hidden lg:grid lg:grid-cols-2 gap-6 p-4 h-screen">
+  //       <div className="overflow-y-auto">
+  //         <Preview />
+  //       </div>
+  //       <div className="overflow-y-auto">
+  //         <MainForm />
+  //       </div>
+  //     </div>
+
+  //     {/* ---------- Small/Medium Screens: Toggle View ---------- */}
+  //     <div className="block lg:hidden relative h-screen overflow-hidden">
+  //       <div
+  //         className={`transition-transform duration-500 ease-in-out h-full ${
+  //           showPreview ? "translate-y-full" : "translate-y-0"
+  //         }`}
+  //       >
+  //         {/* Main Form (visible when preview hidden) */}
+  //         <MainForm />
+  //         {/* <button
+  //           type="button"
+  //           onClick={() => setShowPreview(true)}
+  //           className="fixed bottom-4 right-4 bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md"
+  //         >
+  //           Check Preview
+  //         </button> */}
+
+  //         <DraggableBtn
+  //           btnLabel="Check Preview"
+  //           onClick={() => setShowPreview(true)}
+  //         />
+  //       </div>
+
+  //       {/* Preview (slides up from bottom)  */}
+  //       <div
+  //         className={`absolute inset-0 bg-white z-50 overflow-y-auto transition-transform duration-500 ease-in-out ${
+  //           showPreview ? "translate-y-0" : "translate-y-full"
+  //         }`}
+  //       >
+  // <button
+  //   type="button"
+  //   onClick={() => setShowPreview(false)}
+  //   className="absolute top-3 right-3 bg-red-500 rounded-full p-2"
+  // >
+  //   ✕
+  // </button>
+  //         <Preview />
+  //       </div>
+  //     </div>
+  //   </FormProvider>
+  // );
+
+  /*TRY 3*/
   return (
     <FormProvider {...methods}>
-      {/* Large screens: show both form and preview side by side */}
-      <div className="hidden lg:grid lg:grid-cols-2 gap-6 p-4 h-screen">
-        <div className="overflow-y-auto">
-          <Preview />
-        </div>
-        <div className="overflow-y-auto">
-          <MainForm />
-        </div>
-      </div>
-
-      {/* ---------- Small/Medium Screens: Toggle View ---------- */}
-      <div className="block lg:hidden relative h-screen overflow-hidden">
-        <div
-          className={`transition-transform duration-500 ease-in-out h-full ${
-            showPreview ? "translate-y-full" : "translate-y-0"
-          }`}
+      <div className="global-container grid relative lg:grid-cols-2 md:grid-cols-1 gap-5 p-4 h-screen overflow-hidden">
+        <section
+          className={`preview-container lg:order-1 overflow-y-auto h-screen 
+            ${isPreviewActive ? "order-1" : "order-2"}`}
         >
-          {/* Main Form (visible when preview hidden) */}
-          <MainForm />
-          {/* <button
-            type="button"
-            onClick={() => setShowPreview(true)}
-            className="fixed bottom-4 right-4 bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md"
-          >
-            Check Preview
-          </button> */}
-
-          <DraggableBtn
-            btnLabel="Check Preview"
-            onClick={() => setShowPreview(true)}
-          />
-        </div>
-
-        {/* Preview (slides up from bottom)  */}
-        <div
-          className={`absolute inset-0 bg-white z-50 overflow-y-auto transition-transform duration-500 ease-in-out ${
-            showPreview ? "translate-y-0" : "translate-y-full"
-          }`}
-        >
-          <button
-            type="button"
-            onClick={() => setShowPreview(false)}
-            className="absolute top-3 right-3 bg-red-500 rounded-full p-2"
-          >
-            ✕
-          </button>
+          {isPreviewActive && (
+            <button
+              type="button"
+              onClick={() => setPreviewActive(false)}
+              className="absolute top-3 right-3 bg-red-500 rounded-full p-2 block lg:hidden"
+            >
+              ✕
+            </button>
+          )}
           <Preview />
-        </div>
+        </section>
+
+        <section
+          className={`inputForm-container lg:order-2 overflow-y-auto h-screen block
+             ${isPreviewActive ? "order-2" : "order-1"}
+            `}
+        >
+          {!isPreviewActive && (
+            <DraggableBtn
+              btnLabel="Check Preview"
+              onClick={() => setPreviewActive(true)}
+              btnStyle="block lg:hidden
+              "
+            />
+          )}
+          <MainForm />
+        </section>
       </div>
     </FormProvider>
   );
